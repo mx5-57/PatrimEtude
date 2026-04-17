27644e5f584063bc20996ff494e58de1cb43f3e1
mx5-57 <jc.chevalme@gmail.com>
2026-04-17 09:29:01 +0200

    Update build.rs

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,6 @@
 fn main() {
-  tauri_build::build()
+  // Utilise try_build pour eviter l'echec si les icones sont absentes
+  if let Err(e) = tauri_build::try_build(tauri_build::Attributes::new()) {
+    println!("cargo:warning=tauri-build warning: {}", e);
+  }
 }
